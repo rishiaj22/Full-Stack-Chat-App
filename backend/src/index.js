dae59adf78b86b107f7009281278487ec3e24034
rishiaj22 <shrivastavarishiraj198@gmail.com>
2025-04-14 13:15:34 +0530

    Chat Application

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 
